00c805c47295c7c847d8ae948576fc36e5092273
aaron <achew012@e.ntu.edu.sg>
2022-03-04 17:12:38 +0800

    fixed template bugs

--- src/run_qg.py
+++ src/run_qg.py
@@ -8,15 +8,16 @@ import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
 from clearml import Task, StorageManager
 
-Task.add_requirements('rouge_score')
-Task.add_requirements('nltk')
+Task.add_requirements(package_name="datasets", package_version="1.18.3")
+Task.add_requirements("rouge_score")
+Task.add_requirements("nltk")
 
 
 def get_clearml_params(task: Task) -> Dict[str, Any]:
-    '''
+    """
     returns task params as a dictionary
     the values are casted in the required Python type
-    '''
+    """
     string_params = task.get_parameters_as_dict()
     clean_params = {}
     for k, v in string_params["General"].items():
@@ -38,12 +39,16 @@ def train(cfg, task) -> LongformerQG:
         mode="min",
         save_top_k=1,
         save_weights_only=True,
-        period=5
+        period=5,
     )
 
     model = LongformerQG(cfg, task)
-    trainer = pl.Trainer(gpus=1, max_epochs=cfg.num_epochs, accumulate_grad_batches=cfg.grad_accum,
-                         callbacks=[checkpoint_callback])
+    trainer = pl.Trainer(
+        gpus=1,
+        max_epochs=cfg.num_epochs,
+        accumulate_grad_batches=cfg.grad_accum,
+        callbacks=[checkpoint_callback],
+    )
     trainer.fit(model)
     return model
 
@@ -60,11 +65,17 @@ def hydra_main(cfg) -> float:
     print("Detected config file, initiating task... {}".format(cfg))
 
     if cfg.train:
-        task = Task.init(project_name='LongQG', task_name='longQG-train',
-                         output_uri="s3://experiment-logging/storage/")
+        task = Task.init(
+            project_name="LongQG",
+            task_name="longQG-train",
+            output_uri="s3://experiment-logging/storage/",
+        )
     else:
-        task = Task.init(project_name='LongQG', task_name='longQG-predict',
-                         output_uri="s3://experiment-logging/storage/")
+        task = Task.init(
+            project_name="LongQG",
+            task_name="longQG-predict",
+            output_uri="s3://experiment-logging/storage/",
+        )
 
     cfg_dict = OmegaConf.to_container(cfg, resolve=True)
     task.connect(cfg_dict)
@@ -78,10 +89,10 @@ def hydra_main(cfg) -> float:
 
     if cfg.test:
         if cfg.trained_model_path:
-            trained_model_path = StorageManager.get_local_copy(
-                cfg.trained_model_path)
+            trained_model_path = StorageManager.get_local_copy(cfg.trained_model_path)
             model = LongformerQG.load_from_checkpoint(
-                trained_model_path, cfg=cfg, task=task)
+                trained_model_path, cfg=cfg, task=task
+            )
 
         results = test(cfg, model)
 
